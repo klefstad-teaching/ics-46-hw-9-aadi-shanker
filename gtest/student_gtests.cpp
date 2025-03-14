4eec9b2cc8e5362349d8d910c21cbef2be76c7c7
Aadi Shanker <aadidevs@uci.edu>
2025-03-14 21:24:16 +0000

    gtests implementation

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -3,6 +3,49 @@
 #include "dijkstras.h"
 #include "ladder.h"
 
-TEST(Replace, Me) {
-  EXPECT_TRUE(false);
+#include <set>
+#include <string>
+#include <vector>
+
+
+TEST(LadderTest, CatToDog) {
+  set<string> word_list;
+  load_words(word_list, "src/words.txt");
+  vector<string> ladder = generate_word_ladder("cat", "dog", word_list);
+  EXPECT_EQ(ladder.size(), 4);
+}
+
+TEST(LadderTest, MartyToCurls) {
+  set<string> word_list;
+  load_words(word_list, "src/words.txt");
+  vector<string> ladder = generate_word_ladder("marty", "curls", word_list);
+  EXPECT_EQ(ladder.size(), 6);
+}
+
+TEST(LadderTest, CodeToData) {
+  set<string> word_list;
+  load_words(word_list, "src/words.txt");
+  vector<string> ladder = generate_word_ladder("code", "data", word_list);
+  EXPECT_EQ(ladder.size(), 6);
+}
+
+TEST(LadderTest, WorkToPlay) {
+  set<string> word_list;
+  load_words(word_list, "src/words.txt");
+  vector<string> ladder = generate_word_ladder("work", "play", word_list);
+  EXPECT_EQ(ladder.size(), 6);
+}
+
+TEST(LadderTest, SleepToAwake) {
+  set<string> word_list;
+  load_words(word_list, "src/words.txt");
+  vector<string> ladder = generate_word_ladder("sleep", "awake", word_list);
+  EXPECT_EQ(ladder.size(), 8);
+}
+
+TEST(LadderTest, CarToCheat) {
+  set<string> word_list;
+  load_words(word_list, "src/words.txt");
+  vector<string> ladder = generate_word_ladder("car", "cheat", word_list);
+  EXPECT_EQ(ladder.size(), 4);
 }
